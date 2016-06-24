909234a98ccc257e683215a260e439e32aa86b2c
Nick Fenwick <neek@nickfenwick.com>
2016-06-24 10:01:23 +0700

    Remove unnecessary backticks.

--- offscreen-window-restore.sh
+++ offscreen-window-restore.sh
@@ -7,10 +7,10 @@
 
 width=`xrandr | grep current | awk {'print $8'}`
 
-`wmctrl -l -G | awk -v w=$width '{
+wmctrl -l -G | awk -v w=$width '{
 	if ($8 != "unity-dash" && $8 != "Hud") {
 		if ($3 >= w || $3 < 0) {
 			system("wmctrl -i -r " $1 " -e 0," sqrt($3*$3) % w ",-1,-1,-1");
 		}
 	}
-}'`
+}'
